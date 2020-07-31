38ac922c64b526795c965216c0786293655f2948
VladisLuv <mukpo1kg@gmail.com>
2020-07-31 10:20:20 +0300

    react practice

--- src/index.js
+++ src/index.js
@@ -1,8 +1,38 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import App from './components/app/';
+
+
+class WhoAmI extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            years: 26
+        }
+    }
+
+    nextYear(){
+        console.log(1)
+    }
+
+    render() {
+        const {name, surname, link} = this.props;
+        const {years} = this.state;
+        return (
+            <>
+                <button onClick={this.nextYear}> ++ </button>
+                <h1>
+                    My name is {name}, surname - {surname}, years = {years}
+                </h1>
+                <a href={link}>My profile</a>
+            </>
+        )
+    }
+}
+
+
+
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,7 +41,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
