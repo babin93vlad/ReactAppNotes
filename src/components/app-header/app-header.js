b3a2b097d6e7f4c805711a7e523a7afa0c463fd0
VladisLuv <mukpo1kg@gmail.com>
2020-07-31 10:22:04 +0300

    some components project

---
+++ src/components/app-header/app-header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./app-header.css";
+
+const AppHeader = () => {
+    return (
+        <div className="app-header d-flex">
+            <h1>Vladislav Babin</h1>
+            <h2>5 notes, 0 like</h2>
+        </div>
+    )
+}
+
+export default AppHeader;
---
+++ src/components/post-add-form/post-add-form.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./post-add-form.css";
+
+const PostAddForm = () => {
+    return (
+        <form className="bottom-panel d-flex">
+            <input
+                type="text"
+                placeholder="What you fink about?"
+                className="form-control new-post-label"
+            />
+            <button
+                type="submit"
+                className="btn btn-outline-secondary"
+            >
+                Add note
+            </button>
+
+        </form>
+    )
+}
+
+export default PostAddForm;
